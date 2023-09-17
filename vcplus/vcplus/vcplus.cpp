b336f0daf2d42676c5d2a6663d38a574a77a5d3c
kjunh972 <kjunh972@gmail.com>
2023-09-17 15:29:21 +0900

    2week

--- vcplus/vcplus/vcplus.cpp
+++ vcplus/vcplus/vcplus.cpp
@@ -4,50 +4,57 @@
 #include <conio.h>
 
 #define ESC 27
-
-
+using namespace std;
 
 int main()
 {
+    printf("김준형의 행복한 VC++");
+    printf("\n");
 
+    while (1)
+    {
+        char c;
+        printf("command>");
 
-	printf("김준형의 행복한 VC++");
-	printf("\n");
+        // 문자열을 저장할 배열
+        char input[100];
+        int index = 0;
 
-	while (1)
-	{
-		char s;
-		printf("command>");
+        while (1)
+        {
+            c = _getch();
+            if (c == 27) {
+                return 0;;
+            }
+            else if (c == '\r') {
+                input[index] = '\0'; 
+                break;
+            }
+            else {
+                printf("%c", c);
+                input[index++] = c;
+            }
+        }
+        printf("\n");
 
-
-
-		s = _getche();
-		if (s == 27) {
-			break;
-		}
-		while (1) {
-			char enter = _getch();
-			if (enter == '\r')
-				break;
-		}
-		printf("\n");
-
-		if (s == 'q') {
-			printf("q:qwer화이팅\n");
-
-		}
-		else if (s == 'w') {
-			printf("w:과제 너무 좋다\n");
-		}
-		else if (s == 'e') {
-			printf("e:담주부턴 과제량 3배다\n");
-		}
-		else if (s == 'r') {
-			printf("r:행복합니다\n");
-		}
-
-		else {
-			printf("잘못 입력하셨습니다. q,w,e,r중 입력해주세요.\n");
-		}
-	}
+        if (strcmp(input, "q") == 0) {
+            printf("q: qwer 화이팅\n");
+        }
+        else if (strcmp(input, "w") == 0) {
+            printf("w: 과제 너무 좋다\n");
+        }
+        else if (strcmp(input, "e") == 0) {
+            printf("e: 담주부턴 과제량 3배다\n");
+        }
+        else if (strcmp(input, "r") == 0) {
+            printf("r: 행복합니다\n");
+        }
+        else if (strcmp(input, "exit") == 0) {
+            break;
+        }
+        else {
+            printf("잘못 입력하셨습니다. q, w, e, r 중 입력해주세요.\n");
+        }
+    }
+    return 0;
 }
